251615f9b97deaacf265f613c9c26dfee4373da5
SialasTripp <sstripp@nic.edu>
2023-10-31 20:09:58 -0700

    initial reticle sprite created to replace cursor

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 #include <SFML/Graphics.hpp>
 #include "background.h"
 
-int main(){
+
+int main()
+{
 
     sf::RenderWindow window(sf::VideoMode(640, 480), "Big Buck Hunter");
     Background background;
 
+    // hides system mouse cursor
+    window.setMouseCursorVisible(false);
+
+    
+    //change the mouse cursor to reticle
+    sf::Texture texture;
+    texture.loadFromFile("images/reticle.png");
+    sf::Sprite reticle(texture);
+    reticle.setOrigin(reticle.getLocalBounds().width / 2, reticle.getLocalBounds().height / 2);
+
+    // sets scale of reticle to be smaller
+    float scaleFactor = .25f; 
+    reticle.setScale(scaleFactor, scaleFactor);
+   
     while (window.isOpen())
     {
         sf::Event event;
@@ -14,9 +30,14 @@ int main(){
             if (event.type == sf::Event::Closed)
             window.close();
         }
-    window.clear();
-    window.draw(background.mSprite);
-    window.display();
+        sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
+        reticle.setPosition(static_cast<sf::Vector2f>(mousePosition));
+
+        window.clear();
+        window.draw(background.mSprite);
+        window.draw(reticle);
+        window.display();
     }
+
     return 0;
 }
