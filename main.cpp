9020f69b0eb218170f7aa5ecd6b2cb394805b0eb
SialasTripp <sstripp@nic.edu>
2023-11-05 09:54:31 -0800

    added reticle class

--- main.cpp
+++ main.cpp
@@ -2,30 +2,20 @@
 #include "background.h"
 #include "coverArt.h"
 #include "gameover.h"
-
+#include "reticle.h"
 
 int main()
 {
 
     sf::RenderWindow window(sf::VideoMode(640, 480), "Big Buck Hunter");
     Background background;
+    Reticle reticle;
     CoverArt coverArt;
     GameOver gameOver;
 
     // hides system mouse cursor
     window.setMouseCursorVisible(false);
 
-    
-    //change the mouse cursor to reticle
-    sf::Texture texture;
-    texture.loadFromFile("images/reticle.png");
-    sf::Sprite reticle(texture);
-    reticle.setOrigin(reticle.getLocalBounds().width / 2, reticle.getLocalBounds().height / 2);
-
-    // sets scale of reticle to be smaller
-    float scaleFactor = .25f; 
-    reticle.setScale(scaleFactor, scaleFactor);
-   
     while (window.isOpen())
     {
         sf::Event event;
@@ -40,14 +30,14 @@ int main()
             // }
         }
         sf::Vector2i mousePosition = sf::Mouse::getPosition(window);
-        reticle.setPosition(static_cast<sf::Vector2f>(mousePosition));
+        reticle.mSprite.setPosition(static_cast<sf::Vector2f>(mousePosition));
 
         window.clear();
         window.draw(background.mSprite);
         window.draw(coverArt.deerSprite);
         window.draw(coverArt.text);
         window.draw(coverArt.text2);
-        window.draw(reticle);
+        window.draw(reticle.mSprite);
 
         //Game Over Image
         // window.draw(gameOver.sprite);
